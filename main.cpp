7a343e33abc626faa1060c16075b1bad950cb40a
JACK LIN <cvse00566@gmail.com>
2025-02-01 20:24:40 +0800

    變數交換

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
+    int num1, num2, tmp;
+
+    cout << "input two num" << endl;
+    cin >> num1 >> num2;
+
+    cout << "=== line ===" << endl;
+
+    tmp =  num1;
+    num1 =  num2;
+    num2 =  tmp;
+    cout << num1 << " " << num2;
+
     return 0;
 }

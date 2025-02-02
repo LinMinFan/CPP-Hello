3413386f73779dfb11692ea8f04a824d294ce2d6
JACK LIN <cvse00566@gmail.com>
2025-02-02 11:40:11 +0800

    for 迴圈用*畫出菱形

--- main.cpp
+++ main.cpp
@@ -3,6 +3,33 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
+    int n = 7, space, start;
+    int mid = (n + 1) / 2;
+
+    for (int i = 1; i <= 7; i++)
+    {
+        if (i <= mid)
+        {
+            space = mid - i;
+            start = (i * 2) - 1;
+        } else
+        {
+            space = i - mid;
+            start = ((n + 1 - i) * 2) - 1;
+        }
+
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
+        }
+
+        for (int k = 1; k <= start; k++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+    
     return 0;
 }

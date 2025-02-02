d16256e309a39415460724a9b4240f9a006ad11b
JACK LIN <cvse00566@gmail.com>
2025-02-02 11:00:15 +0800

    for 迴圈計算階層總和

--- main.cpp
+++ main.cpp
@@ -3,6 +3,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
+    int num, factorial, sum;
+    sum = 0;
+
+    cout << "enter a num" << endl;
+    cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        factorial = 1;
+
+        for (int j = 1; j <= i; j++)
+        {
+            factorial *= j;
+        }
+
+        sum +=  factorial;
+    }
+
+    cout << "sum is : " << sum << endl;
+    
     return 0;
 }
